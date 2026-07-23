26eb11daf830e0b368accd8283ce76471779dc24
Willian <williansavio98@gmail.com>
2026-07-22 21:25:39 -0300

    feat - merge

---
+++ 3-iniciar-painel-electron.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "C:\Users\larissa.rodrigues18\Documents\UC12\Electron\painel-policia-electron"
+echo.
+echo ╔════════════════════════════════════════╗
+echo ║  🚔 INICIANDO PAINEL POLÍCIA - DESKTOP║
+echo ║  Aplicação Electron                    ║
+echo ║  Certifique-se que o Backend está ON  ║
+echo ╚════════════════════════════════════════╝
+echo.
+npm start
+pause
